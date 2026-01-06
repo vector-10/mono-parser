81e4febfe2db4d1bee6700200bc4ab8a62b94405
vector-10 <duzieblaise10@gmail.com>
2026-01-06 13:50:09 +0100

    chore: updates to the application

--- apps/gateway/src/app/app.controller.ts
+++ apps/gateway/src/app/app.controller.ts
@@ -1,12 +1,34 @@
 import { Controller, Get } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly prismaService: PrismaService) {}
 
   @Get()
   getData() {
     return this.appService.getData();
+  }
+
+  @Get('test-db')
+  async checkDatabase() {
+    try {
+      // This is a "ping" to the database
+      await this.prismaService.$queryRaw`SELECT 1`;
+      console.log(' Manual DB Check: Connection is healthy.');
+      return {
+        status: 'Online',
+        message: 'The Gateway is successfully connected to the Docker Postgres database!',
+        timestamp: new Date().toISOString(),
+      };
+    } catch (error:any) {
+      console.error(' Manual DB Check Failed:', error.message);
+      return {
+        status: 'Offline',
+        message: 'Database connection failed',
+        error: error.message,
+      };
+    }
   }
 }
--- apps/gateway/src/app/app.module.ts
+++ apps/gateway/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from '../prisma.service';
+import { PrismaService } from './prisma.service';
+
 
 @Module({
   imports: [],
