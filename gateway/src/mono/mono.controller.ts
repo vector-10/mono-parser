67eeb4b66ca0da7198421773892868dc3ba245c5
vector-10 <duzieblaise10@gmail.com>
2026-01-15 17:59:01 +0100

    chore: updates to mono controller

--- gateway/src/mono/mono.controller.ts
+++ gateway/src/mono/mono.controller.ts
@@ -2,12 +2,14 @@ import {
   Controller,
   Request,
   Post,
+  Get,
+  Body,
+  Param,
   UseGuards,
   BadRequestException,
 } from '@nestjs/common';
 import { MonoService } from './mono.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
-
 
 @Controller('mono')
 @UseGuards(JwtAuthGuard)
@@ -26,5 +28,48 @@ export class MonoController {
       req.user.email,
       req.user.monoApiKey,
     );
+  }
+
+  @Get('account/:accountId')
+  async getDetails(@Request() req, @Param('accountId') accountId: string) {
+    return this.monoService.getAccountDetails(accountId, req.user.monoApiKey);
+  }
+
+  @Get('account/:accountId/balance')
+  async getBalance(@Request() req, @Param('accountId') accountId: string) {
+    return this.monoService.getAccountBalance(accountId, req.user.monoApiKey);
+  }
+
+  @Get('account/:accountId/income')
+  async getIncome(@Request() req, @Param('accountId') accountId: string) {
+    return this.monoService.getIncome(accountId, req.user.monoApiKey);
+  }
+
+  @Get('account/:accountId/insights')
+  async getInsights(@Request() req, @Param('accountId') accountId: string) {
+    return this.monoService.getStatementInsights(accountId, req.user.monoApiKey);
+  }
+
+  @Post('account/:accountId/creditworthiness')
+  async checkCredit(
+    @Request() req,
+    @Param('accountId') accountId: string,
+    @Body() loanData: any,
+  ) {
+    return this.monoService.getCreditWorthiness(
+      accountId,
+      req.user.monoApiKey,
+      loanData,
+    );
+  }
+
+  @Post('lookup/credit-history')
+  async getHistory(@Request() req, @Body('bvn') bvn: string) {
+    return this.monoService.getCreditHistory(bvn, req.user.monoApiKey);
+  }
+
+  @Post('account/:accountId/unlink')
+  async unlink(@Request() req, @Param('accountId') accountId: string) {
+    return this.monoService.unlinkAccount(accountId, req.user.monoApiKey);
   }
 }
