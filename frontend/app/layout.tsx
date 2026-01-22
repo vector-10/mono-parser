dd11c3170525f06667f426207ed49f561a10f1f4
vector-10 <duzieblaise10@gmail.com>
2026-01-22 21:25:23 +0100

    chore: updates to frontend

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Mono Parser",
+  description: "More Reasons to say No or Yes to a Loan",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ frontend/app/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const LandingPage = () => {
+  <><div className="text-white">Hi</div></>;
+};
+
+
+export default LandingPage
