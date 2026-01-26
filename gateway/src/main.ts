9983b127d458ec23a284fd112f97e6fcf9728690
vector-10 <duzieblaise10@gmail.com>
2026-01-26 08:46:38 +0100

    chore: port for backend updated

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
   app.flushLogs();
 
   app.setGlobalPrefix('api');
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
