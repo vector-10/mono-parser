b4d6afa24c33cc28955b66b90011e4fec9df186e
vector-10 <duzieblaise10@gmail.com>
2026-02-17 14:47:57 +0100

    chore: updates to create applicant dto with decorators

--- gateway/src/applicants/dto/create-applicant.dto.ts
+++ gateway/src/applicants/dto/create-applicant.dto.ts
@@ -1,7 +1,26 @@
+import { IsEmail, IsString, MinLength, MaxLength } from 'class-validator';
+
 export class CreateApplicantDto {
+  @IsString()
+  @MinLength(2)
+  @MaxLength(100)
   firstName: string;
+
+  @IsString()
+  @MinLength(2)
+  @MaxLength(100)
   lastName: string;
+
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @MinLength(10)
+  @MaxLength(15)
   phone?: string;
+
+  @IsString()
+  @MinLength(11)
+  @MaxLength(11)
   bvn?: string;
 }
